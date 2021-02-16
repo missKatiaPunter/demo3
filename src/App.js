91ddc578fc98a8afaac53ede463f2cf32a71b33f
Katia <katia.punter@storyshareplatform.com>
2021-02-16 15:00:08 +0000

    Add things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <Counter/>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [ counter, setCounter ] = useState(0);
+    return(
+        <button
+            onClick = { () => setCounter(counter+1)}
+            data-testid = 'counter-butt'
+        >{ counter }
+        </button>
+    )
+}
+
+export default Counter;
